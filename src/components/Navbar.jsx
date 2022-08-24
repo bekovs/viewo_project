f719de4d5382ce0dfc9ca9d7ac3b511a42d16d57
Qwery <shamievmalik@gmail.com>
2022-08-24 15:32:13 +0600

    Navbar finish

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState } from 'react';
+import "../styles/navbar.css"
+
+const Navbar = () => {
+
+
+
+
+
+  return (
+    <header>
+      <div className="container">
+        <div className="header__logo">
+          <a>
+            <img src="https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn" alt="" className='header-logo' />
+          </a>
+        </div>
+        <div className="header__search">
+          <input type="text" placeholder='Search accounts and videos' />
+          <button>Search</button>
+        </div>
+
+        <div className="header__block-right">
+          <div className="header__profile_section">
+            <div className="header__chat-icon">
+              <img src="https://play-lh.googleusercontent.com/cF_oWC9Io_I9smEBhjhUHkOO6vX5wMbZJgFpGny4MkMMtz25iIJEh2wASdbbEN7jseAx" alt="" />
+            </div>
+            <div className="header__profile-avatar" >
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png" alt="" />
+            </div>
+            <div
+              className="modal-profile"
+            >
+              <ul>
+                <li><a>View profile</a></li>
+                <li><a>Settings</a></li>
+                <li><a>English</a></li>
+                <li><a>Log out</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="header__login">
+            <button className="header__btn-login">
+              Log in
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
